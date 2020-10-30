adb5932b726460672ac226885f792a28c86962de
Seungjun Moon <72376931+SeungjunMoon@users.noreply.github.com>
2020-10-30 21:49:52 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+//CSS와 별개로 픽셀 컨트롤을 위한 캔버스 사이즈 지정 필요
+canvas.width = 700;
+canvas.height = 700;
+
+//context mdn 문서 참고
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false; //기본값 설정
 
@@ -6,28 +15,30 @@ function stopPainting() {
   painting = false;
 }
 
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
-function onMouseLeave(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
 
